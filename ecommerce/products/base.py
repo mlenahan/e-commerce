d424195e255c9511a278ae4caca9f3367f9d6f9b
m_lenahan <michael_lenahan@hotmail.com>
2021-02-25 14:56:20 +0000

    applying feedback on models and base

--- ecommerce/products/base.py
+++ ecommerce/products/base.py
@@ -2,12 +2,20 @@ from django.db import models
 
 
 class Rarity:
-    COMMON = 'common'
-    UNCOMMON = 'uncommon'
-    RARE = 'rare'
-    ULTRA_RARE = 'ultra rare'
-    AMAZING_RARE = 'amazing rare'
-    SECRET_RARE = 'secret rare'
+    COMMON = 'Common'
+    UNCOMMON = 'Uncommon'
+    RARE = 'Rare'
+    ULTRA_RARE = 'Ultra Rare'
+    AMAZING_RARE = 'Amazing Rare'
+    SECRET_RARE = 'Secret Rare'
+    CHOICES = [
+        (COMMON, 'Common'),
+        (UNCOMMON, 'Uncommon'),
+        (RARE, 'Rare'),
+        (ULTRA_RARE, 'Ultra Rare'),
+        (AMAZING_RARE, 'Amazing Rare'),
+        (SECRET_RARE, 'Secret Rare')
+    ]
 
 class Languages:
     ENGLISH = 'English'
@@ -17,28 +25,58 @@ class Languages:
     GERMAN = 'German'
     CHINESE ='Chinese'
     KOREAN = 'Korean'
+    CHOICES = [(ENGLISH, 'English'),
+               (JAPANESE, 'Japanese'),
+               (FRENCH, 'French'),
+               (SPANISH, 'Spanish'),
+               (GERMAN, 'German'),
+               (CHINESE, 'Chinese'),
+               (KOREAN, 'Korean')
+               ]
 
 class Condition:
-    SEALED_PRODUCT = 'sealed product'
-    PACK_FRESH = 'pack fresh'
-    LIGHT_PLAY = 'light play'
-    MODERATE_PLAY = 'moderate play'
-    HEAVY_PLAY = 'heavy play'
-    DAMAGED = 'damaged'
+    SEALED_PRODUCT = 'Sealed Product'
+    PACK_FRESH = 'Pack Fresh'
+    LIGHT_PLAY = 'Light Play'
+    MODERATE_PLAY = 'Moderate Play'
+    HEAVY_PLAY = 'Heavy Play'
+    DAMAGED = 'Damaged'
+    CHOICES = [(SEALED_PRODUCT, 'Sealed Product'),
+               (PACK_FRESH, 'Pack Fresh'),
+               (LIGHT_PLAY, 'Light Play'),
+               (MODERATE_PLAY, 'Moderate Play'),
+               (HEAVY_PLAY, 'Heavy Play'),
+               (DAMAGED, 'Damaged')
+               ]
 
 class ProductType:
-    BOOSTER_BOX = 'booster box'
-    ELITE_TRAINER_BOX_PLUS = 'elite trainer box plus'
-    ELITE_TRAINER_BOX = 'elite trainer box'
-    PREMIUM_COLLECTION_BOX 'premium collection box'
-    POKEMON_THEMED_BOX = 'pokemon themed box'
-    PIN_BOX = 'pin box'
-    THEME_DECK = 'theme deck'
-    BUILD_AND_BATTLE_DECK = 'build and battle deck'
-    PREMIUM_BLISTER = 'premium blister'
-    THREE_PACK_BLISTER = 'three pack blister'
-    SINGLE_PACK_BLISTER = 'single pack blister'
-    BOOSTER_PACK = 'booster pack'
-    SINGLE_CARD = 'single card'
+    BOOSTER_BOX = 'Booster Box'
+    ELITE_TRAINER_BOX_PLUS = 'Elite Trainer Box Plus'
+    ELITE_TRAINER_BOX = 'Elite Trainer Box'
+    PREMIUM_COLLECTION_BOX = 'Premium Collection Box'
+    POKEMON_THEMED_BOX = 'Pokemon Themed Box'
+    PIN_BOX = 'Pin Box'
+    THEME_DECK = 'Theme Deck'
+    BUILD_AND_BATTLE_DECK = 'Build and Battle Deck'
+    PREMIUM_BLISTER = 'Premium Blister'
+    THREE_PACK_BLISTER = 'Three Pack Blister'
+    SINGLE_PACK_BLISTER = 'Single Pack Blister'
+    BOOSTER_PACK = 'Booster Pack'
+    SINGLE_CARD = 'Single Card'
+    CHOICES = [
+        (BOOSTER_BOX, 'Booster Box'),
+        (ELITE_TRAINER_BOX_PLUS, 'Elite Trainer Box Plus'),
+        (ELITE_TRAINER_BOX, 'Elite Trainer Box'),
 
+        (PREMIUM_COLLECTION_BOX, 'Premium Collection Box'),
+        (POKEMON_THEMED_BOX, 'Pokemon Themed Box'),
+        (PIN_BOX, 'Pin Box'),
+        (THEME_DECK, 'Theme Deck'),
+        (BUILD_AND_BATTLE_DECK, 'Build and Battle Deck'),
+        (PREMIUM_BLISTER, 'Premium Blister'),
+        (THREE_PACK_BLISTER, 'Three Pack Blister'),
+        (SINGLE_PACK_BLISTER, 'Single Pack Blister'),
+        (BOOSTER_PACK, 'Booster Pack'),
+        (SINGLE_CARD, 'Single Card')
+    ]
 
